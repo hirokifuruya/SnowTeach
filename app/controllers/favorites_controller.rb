6563edf1b92bd3b18bd446c96bd3c5e5a873b5dc
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2023-02-19 00:59:38 +0900

    フォロー機能実装完了

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,10 +3,14 @@ class FavoritesController < ApplicationController
 
 
   def create
-    @user = User.find(params[:followed_id])
-    current_user.active_favorites.create(followed_id: @user.id)
+    @user = User.find(params[:favorite][:followed_id])
+    if current_user.id != @user.id # 自分自身をフォローできないようにする
+      current_user.active_favorites.create(followed_id: @user.id)
+    end
     redirect_to @user
   end
+
+
 
   def destroy
     @user = Favorite.find(params[:id]).followed
