f67b3ce773aaffdc8d1d739409cd99e3624568c9
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2023-02-22 02:42:18 +0900

    お気に入り機能途中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,20 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :set_user
 
   def create
-    @user = User.find(params[:favorite][:follower_id])
-    if current_user.id != @user.id # 自分自身をお気に入りできないようにする
-      current_user.favorites.create(followed_id: @user.id)
-    end
-    redirect_to current_user
+    current_user.follow(@user)
+    redirect_back(fallback_location: recruits_path)
   end
 
   def destroy
-    @user = Favorite.find(params[:id]).followed
-    current_user.favorites.find_by(followed_id: @user.id).destroy
-    redirect_to current_user
+    current_user.unfollow(@user)
+    redirect_back(fallback_location: recruits_path)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
+
