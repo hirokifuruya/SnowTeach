68f85807d725fcc5fe525a6851547ce32004169d
古谷啓起 <furuyahiroki@furuyakeikinoMacBook-Air.local>
2023-02-25 18:32:12 +0900

    ボディとフッダー調節

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,52 +9,52 @@
 
 
 #<roleデータ>
-Role.create!(name: "admin") unless Role.exists?(name: "admin")
-Role.create!(name: "instructor") unless Role.exists?(name: "instructor")
-Role.create!(name: "general") unless Role.exists?(name: "general")
+# Role.create!(name: "admin") unless Role.exists?(name: "admin")
+# Role.create!(name: "instructor") unless Role.exists?(name: "instructor")
+# Role.create!(name: "general") unless Role.exists?(name: "general")
 
 
 
-#<userデータ>
-User.create!(
-  [
-    {email: 'furuya@gmail.com', password: 'furuya@gmail.com', role_id: 1},
-    {email: 'instructor1@gmail.com', password: 'instructor1@gmail.com', role_id: 2},
-    {email: 'instructor2@gmail.com', password: 'instructor2@gmail.com', role_id: 2},
-    {email: 'general1@gmail.com', password: 'general1@gmail.com', role_id: 3},
-    {email: 'general2@gmail.com', password: 'general2@gmail.com', role_id: 3}
-  ].reject { |user| User.exists?(email: user[:email]) }
-    )
+# #<userデータ>
+# User.create!(
+#   [
+#     {email: 'furuya@gmail.com', password: 'furuya@gmail.com', role_id: 1},
+#     {email: 'instructor1@gmail.com', password: 'instructor1@gmail.com', role_id: 2},
+#     {email: 'instructor2@gmail.com', password: 'instructor2@gmail.com', role_id: 2},
+#     {email: 'general1@gmail.com', password: 'general1@gmail.com', role_id: 3},
+#     {email: 'general2@gmail.com', password: 'general2@gmail.com', role_id: 3}
+#   ].reject { |user| User.exists?(email: user[:email]) }
+#     )
 
-#<skirosortデータ>
-Skiresort.find_or_create_by(id: 1) do |s|
-  s.name = 'かぐらスキー場'
-  s.address = '新潟県南魚沼郡湯沢町三俣742'
-end
+# #<skirosortデータ>
+# Skiresort.find_or_create_by(id: 1) do |s|
+#   s.name = 'かぐらスキー場'
+#   s.address = '新潟県南魚沼郡湯沢町三俣742'
+# end
 
-Skiresort.find_or_create_by(id: 2) do |s|
-  s.name = 'GALA湯沢スキー場'
-  s.address = '新潟県南魚沼郡湯沢町大字湯沢字茅平1039-2'
-end
+# Skiresort.find_or_create_by(id: 2) do |s|
+#   s.name = 'GALA湯沢スキー場'
+#   s.address = '新潟県南魚沼郡湯沢町大字湯沢字茅平1039-2'
+# end
 
-Skiresort.find_or_create_by(id: 3) do |s|
-  s.name = 'ルスツリゾート'
-  s.address = '北海道虻田郡留寿都村字泉川13'
-end
+# Skiresort.find_or_create_by(id: 3) do |s|
+#   s.name = 'ルスツリゾート'
+#   s.address = '北海道虻田郡留寿都村字泉川13'
+# end
 
-Skiresort.find_or_create_by(id: 4) do |s|
-  s.name = 'ニセコ東急 グラン・ヒラフ'
-  s.address = '北海道虻田郡倶知安町ニセコひらふ1条2丁目9番1号'
-end
+# Skiresort.find_or_create_by(id: 4) do |s|
+#   s.name = 'ニセコ東急 グラン・ヒラフ'
+#   s.address = '北海道虻田郡倶知安町ニセコひらふ1条2丁目9番1号'
+# end
 
-Skiresort.find_or_create_by(id: 5) do |s|
-  s.name = 'グランデコスノーリゾート'
-  s.address = '福島県耶麻郡北塩原村大字桧原字荒砂沢山1082番93'
-end
+# Skiresort.find_or_create_by(id: 5) do |s|
+#   s.name = 'グランデコスノーリゾート'
+#   s.address = '福島県耶麻郡北塩原村大字桧原字荒砂沢山1082番93'
+# end
 
 
 
-#<labelデータ>
+# #<labelデータ>
     Label.find_or_create_by(id: 1, name: '初心者' )
     Label.find_or_create_by(id: 2, name: '中級者' )
     Label.find_or_create_by(id: 3, name: '上級者' )
@@ -72,15 +72,15 @@ end
     Label.find_or_create_by(id: 15, name: '中学生' )
     Label.find_or_create_by(id: 16, name: '高校生' )
 
-#<recruitデータ>
-Recruit.find_or_create_by(
-  id: 1,
-  name: '平野歩夢',
-  money: '100,000円',
-  detail: 'オリンピック金メダリスト',
-  start_day: '2023/2/25',
-  end_day: '2023/3/4',
-  skiresort_id: 1,
-  user_id: 2
-)
+# #<recruitデータ>
+# Recruit.find_or_create_by(
+#   id: 1,
+#   name: '平野歩夢',
+#   money: '100,000円',
+#   detail: 'オリンピック金メダリスト',
+#   start_day: '2023/2/25',
+#   end_day: '2023/3/4',
+#   skiresort_id: 1,
+#   user_id: 2
+# )
 
